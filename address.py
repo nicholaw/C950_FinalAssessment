2f08c54dc48eca0a89f396b360f66bebaf8680c2
nicholaw <nicholas.steph.warner@gmail.com>
2021-10-02 10:00:27 -0600

    Added new file for tracking

---
+++ address.py
@@ -0,0 +1,48 @@
+"""
+Represents a physical location on the downtown Salt Lake City map and uses
+a matrix to track the distance between other physical locations on the map
+"""
+class Address:
+	def __init__(self, name):
+		self.name = name
+		self.street = ""
+		self.city = ""
+		self.state = "UT"
+		self.zip = ""
+
+	def of(street, city, state, zip):
+		address = Address("")
+		address.street = street
+		address.city = city
+		address.state = state
+		address.zip = zip
+		return address
+
+	def __str__(self):
+		str = ""
+		if(len(self.name) > 0):
+			str = self.name + "\n" + self.street + "\n" + self.city + ", " + self.state + " " + self.zip
+		else:
+			str = self.street + "\n" + self.city + ", " + self.state + " " + self.zip
+		return str
+
+	def __hash__(self):
+		hash = 2729
+		primeMultiplier = 83
+		string = "" + self.street + self.city + self.state + self.zip
+		for char in string:
+			hash = hash * primeMultiplier + ord(char)
+		return hash % 128
+	
+	def __eq__(self, other):
+		if(type(self) != type(other)):
+			return False
+		if(self.zip != other.zip):
+			return False
+		if(self.state != other.state):
+			return False
+		if(self.city != other.city):
+			return False
+		if(self.street != other.street):
+			return False
+		return True
---
+++ locations.py
@@ -0,0 +1,24 @@
+"""
+Instantiates all possible delivery destinations by reading data from
+resources\destinations.xml
+"""
+
+from address import Address
+import xml.etree.ElementTree as ET
+
+#Create element tree from xml file and instantiate set for storing destinations
+tree = ET.parse("resources/destinations.xml")
+root = tree.getroot()
+allLocations = set()
+
+#Populate set from element tree
+for item in root.findall(".//destination"):
+	destination = Address(item.attrib["name"])
+	for child in item:
+		if(child.tag == "street"):
+			destination.street = child.text
+		elif(child.tag == "city"):
+			destination.city = child.text
+		elif(child.tag == "zip"):
+			destination.zip = child.text
+	allLocations.add(destination)
