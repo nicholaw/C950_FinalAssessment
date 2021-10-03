e90e58ec061ee48e26ed0bf0bdd72263dda4dcd2
nicholaw <nicholas.steph.warner@gmail.com>
2021-10-03 13:00:09 -0600

    finished instantiating set of all possible delivery locations

--- locations.py
+++ locations.py
@@ -11,7 +11,14 @@ tree = ET.parse("resources/destinations.xml")
 root = tree.getroot()
 allLocations = set()
 
-#Populate set from element tree
+#Returns the location with the provided name from the set of all locations
+def get_location(name):
+	for item in allLocations:
+		if(item.name == name):
+			return item
+	return None
+
+#Populate set of all locations from element tree
 for item in root.findall(".//destination"):
 	destination = Address(item.attrib["name"])
 	for child in item:
@@ -24,3 +31,12 @@ for item in root.findall(".//destination"):
 	allLocations.add(destination)
 
 #Define distances among locations
+for item in root.findall(".//destination"):
+	locationA = get_location(item.attrib["name"])
+	for child in item:
+		if(child.tag == "distances"):
+			for grandchild in child:
+				locationB = get_location(grandchild.attrib["name"])
+				distance = float(grandchild.attrib["dist"])
+				locationA.distances[locationB] = distance
+				locationB.distances[locationA] = distance
--- main.py
+++ main.py
@@ -5,5 +5,8 @@ time = Time.of("08:00")
 endTime = Time.of("09:00")
 
 for location in allLocations:
-	print(str(location))
-	print("")
+	if(location.name == "Wheeler Historic Farm"):
+		for item in location.distances:
+			print(item.name)
+			print(str(location.distances[item]))
+			print("")
