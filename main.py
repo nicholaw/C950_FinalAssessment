defd42ca7a026b8701dfa7331e59e60e8d23ec8c
nicholaw <nicholas.steph.warner@gmail.com>
2021-10-05 11:04:13 -0600

    Continued reorganizing truck and packages to make more sence

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from timeofday import Time
 from locations import allLocations
+from packages import allPackages
 
 time = Time.of("08:00")
 endTime = Time.of("09:00")
 
-for location in allLocations:
-	if(location.name == "Wheeler Historic Farm"):
-		for item in location.distances:
-			print(item.name)
-			print(str(location.distances[item]))
-			print("")
+for p in allPackages:
+	print(str(p))
+	print("")
