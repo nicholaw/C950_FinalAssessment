3ef5fad2d38867f8200db7af95f353b66826d7c2
nicholaw <nicholas.steph.warner@gmail.com>
2021-10-08 13:48:07 -0600

    Implemented algorithm for loading farther packages when truck is less than half full and loading closer packages when truck is more than half full, but it's not working

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
 """
-Class which controls interactions between other classes
+Class which controls interactions among other classes
 """
 
 from timeofday import Time
@@ -9,6 +9,7 @@ from truck import Truck
 from constants import MAX_PACKAGES
 
 class Controller:
+	#Constructs this Controller object
 	def __init__(self, numTrucks, startTime, hubName):
 		self.globalTime = startTime
 		self.hub = Controller.set_hub(hubName)
@@ -16,12 +17,14 @@ class Controller:
 		self.populate_fleet(numTrucks)
 		self.packagesDelivered = 0
 	
+	#Sets the hub location based on the provided location name
 	def set_hub(hubName):
 		for location in allLocations:
 			if(location.name == hubName):
 				return location
 		return None
 	
+	#Instantiates the provided number of delivery trucks
 	def populate_fleet(self, numTrucks):
 		if(numTrucks <= 0):
 			self.fleet.add(Truck(1, self.hub, self))
@@ -31,15 +34,59 @@ class Controller:
 				self.fleet.add(Truck(i, self.hub, self))
 				i += 1
 	
-	def load_truck(truck):
-		while((len(truck.cargo) < MAX_PACKAGES) and (len(allPackages) > 0)):
-			truck.load(allPackages.pop())
+	#Loads a package onto a delivery truck;
+	#If the truck is less than half full, loads a package which is farther from the hub than the last package loaded;
+	#If the truck is more than half full, loads a package which is closer to the hub than the last package
+	def load_truck(self, truck):
+		packageToLoad = None
+		if(len(truck.cargo) == 0):
+			packageToLoad = Controller.find_closest(self.hub, allPackages)
+		else:
+			while((len(truck.cargo) < MAX_PACKAGES) and (len(allPackages) > 0)):
+				if(len(truck.cargo) <= (MAX_PACKAGES / 2)):
+					packageToLoad = self.find_next_package(truck.cargo[len(truck.cargo) - 1], False)
+				else:
+					packageToLoad = self.find_next_package(truck.cargo[len(truck.cargo) - 1], True)
+		truck.load(packageToLoad)
+		allPackages.remove(packageToLoad)
+	
+	def find_next_package(self, loadedPagkage, towardsHub):
+		closerToHub = set()
+		fartherFromHub = set()
+		for package in allPackages:
+			if(Controller.find_distance(self.hub, package.dest) <= Controller.find_distance(self.hub, loadedPagkage.dest)):
+				closerToHub.add(package)
+			else:
+				fartherFromHub.add(package)
+		if(towardsHub):
+			if(len(closerToHub) == 0):
+				return find_closest(loadedPagkage.dest, fartherFromHub)
+			else:
+				return find_closest(loadedPagkage.dest, closerToHub)
+		else:
+			if(len(fartherFromHub) == 0):
+				return find_closest(loadedPagkage.dest, closerToHub)
+			else:
+				return find_closest(loadedPagkage.dest, fartherFromHub)
+	
+	def find_closest(locationA, collection):
+		minDist = 999
+		closestPackage = None
+		for item in collection:
+			dist = Controller.find_distance(locationA, item.dest)
+			if(dist < minDist):
+				minDist = dist
+				closestPackage = item
+		return closestPackage
+	
+	def find_distance(locationA, locationB):
+		return allLocations[locationA].distances[locationB]
 	
 	def start(self):
 		while(len(allPackages) > 0):
 			for truck in self.fleet:
 				if(truck.location == self.hub):
-					Controller.load_truck(truck)
+					self.load_truck(truck)
 					truck.make_deliveries()
 				else:
 					truck.check_status()
